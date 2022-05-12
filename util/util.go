2e90182774f98c23caaf14bd899f967a03d3e77d
Cha0sIDL <37327681+Cha0sIDL@users.noreply.github.com>
2022-05-13 00:09:16 +0800

    util

--- util/util.go
+++ util/util.go
@@ -273,3 +273,28 @@ func Unicode2Zh(sText string) []byte {
 	str, _ := strconv.Unquote(strings.Replace(strconv.Quote(textQuoted), `\\u`, `\u`, -1))
 	return []byte(str)
 }
+
+func Unicode2utf8(source string) string {
+	var res = []string{""}
+	sUnicode := strings.Split(source, "\\u")
+	var context = ""
+	for _, v := range sUnicode {
+		var additional = ""
+		if len(v) < 1 {
+			continue
+		}
+		if len(v) > 4 {
+			rs := []rune(v)
+			v = string(rs[:4])
+			additional = string(rs[4:])
+		}
+		temp, err := strconv.ParseInt(v, 16, 32)
+		if err != nil {
+			context += v
+		}
+		context += fmt.Sprintf("%c", temp)
+		context += additional
+	}
+	res = append(res, context)
+	return strings.Join(res, "")
+}
