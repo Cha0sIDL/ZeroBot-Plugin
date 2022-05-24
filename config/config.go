1ec28cc26bc73286766a0d8beb2fd6264798cce8
韩家傲（ChrisHan） <hanjiaao@qiyi.com>
2022-05-24 15:46:17 +0800

    增加奇遇查询接口

--- config/config.go
+++ config/config.go
@@ -19,6 +19,7 @@ type Config struct {
 	Ignore    []string `json:"ignore"`
 	SecretId  string   `json:"secretId"`
 	SecretKey string   `json:"secretKey"`
+	MMToken   string   `json:"MMToken"`
 }
 
 type TTS struct {
--- plugin/jx3/jx3daily.go
+++ plugin/jx3/jx3daily.go
@@ -5,9 +5,11 @@ import (
 	"fmt"
 	"github.com/DanPlayer/timefinder"
 	"github.com/FloatTech/ZeroBot-Plugin/util"
+	"github.com/FloatTech/zbputils/binary"
 	"github.com/FloatTech/zbputils/control"
 	"github.com/FloatTech/zbputils/img/text"
 	"github.com/FloatTech/zbputils/math"
+	"github.com/FloatTech/zbputils/web"
 	"github.com/fogleman/gg"
 	"github.com/golang-module/carbon/v2"
 	log "github.com/sirupsen/logrus"
@@ -653,6 +655,63 @@ func init() {
 			qq := math.Str2Int64(ctx.State["regex_matched"].([]string)[1])
 			deleteLeader(qq)
 			ctx.SendChain(message.At(qq), message.Text("删除成功"))
+		})
+	en.OnPrefixGroup([]string{"奇遇", "奇遇查询"}).SetBlock(true).
+		Handle(func(ctx *zero.Ctx) {
+			var msg string
+			commandPart := util.SplitSpace(ctx.State["args"].(string))
+			if len(commandPart) != 2 {
+				ctx.SendChain(message.Text("参数输入有误！\n" + "奇遇 唯我独尊 柳连柳奶"))
+				return
+			}
+			server := commandPart[0]
+			name := commandPart[1]
+			qiyuUrl := fmt.Sprintf("https://pull.j3cx.com/api/serendipity?server=%s&role=%s&pageIndex=1&pageSize=30", server, name)
+			rspData, err := web.RequestDataWith(web.NewDefaultClient(), qiyuUrl, "GET", "", web.RandUA())
+			if err != nil || gjson.Get(binary.BytesToString(rspData), "code").Int() != 0 {
+				ctx.SendChain(message.Text("出错了联系管理员看看吧"))
+				return
+			}
+			jData := gjson.Get(binary.BytesToString(rspData), "data.data")
+			if len(jData.String()) == 0 {
+				ctx.SendChain(message.Text("没有查到本账号的奇遇呢"))
+				return
+			}
+			for idx, data := range jData.Array() {
+				if idx == 0 {
+					msg += server + "\n"
+				}
+				msg = msg + name + "  " + data.Get("serendipity").String() + "  " + data.Get("date_str").String() + "\n"
+			}
+			ctx.SendChain(message.Text(msg))
+			//var msg string
+			//commandPart := util.SplitSpace(ctx.State["args"].(string))
+			//if len(commandPart) != 2 {
+			//	ctx.SendChain(message.Text("参数输入有误！\n" + "奇遇 唯我独尊 柳连柳奶"))
+			//	return
+			//}
+			//server := commandPart[0]
+			//name := commandPart[1]
+			//qiyuUrl := fmt.Sprintf("https://www.jx3mm.com/home/qyinfo?S=%s&n=%s&u=不限&t=&token=%s", server, name, config.Cfg.MMToken)
+			//rspData, err := util.SendHttp(qiyuUrl, []byte(""))
+			////rspData, err := web.RequestDataWith(web.NewDefaultClient(), qiyuUrl, "GET", "", web.RandUA())
+			////log.Errorln(qiyuUrl, string(rspData), "err", err)
+			//if err != nil || gjson.Get(binary.BytesToString(rspData), "code").Int() != 200 {
+			//	ctx.SendChain(message.Text("出错了联系管理员看看吧"))
+			//	return
+			//}
+			//jData := gjson.Get(binary.BytesToString(rspData), "result")
+			//if len(jData.Array()) == 0 {
+			//	ctx.SendChain(message.Text("没有查到本账号的奇遇呢"))
+			//	return
+			//}
+			//for idx, data := range jData.Array() {
+			//	if idx == 0 {
+			//		msg += server + "\n"
+			//	}
+			//	msg = msg + name + "  " + data.Get("serendipity").String() + "  " + carbon.CreateFromTimestamp(data.Get("time").Int()).ToDateTimeString() + "\n"
+			//}
+			//ctx.SendChain(message.Text(msg))
 		})
 }
 
