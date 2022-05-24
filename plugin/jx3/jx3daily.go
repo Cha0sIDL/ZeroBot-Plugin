46f72edafa6533a133761348fb5a9828581f8f26
Cha0sIDL <37327681+Cha0sIDL@users.noreply.github.com>
2022-05-24 22:33:27 +0800

    加日志

--- plugin/jx3/jx3daily.go
+++ plugin/jx3/jx3daily.go
@@ -695,7 +695,7 @@ func init() {
 			name := commandPart[1]
 			qiyuUrl := fmt.Sprintf("https://www.jx3mm.com/home/qyinfo?S=%s&n=%s&u=不限&t=&token=%s", server, name, config.Cfg.MMToken)
 			rspData, err := web.RequestDataWith(web.NewDefaultClient(), qiyuUrl, "GET", "", web.RandUA())
-			log.Errorln(string(rspData), "err", err)
+			log.Errorln(qiyuUrl, string(rspData), "err", err)
 			if err != nil || gjson.Get(binary.BytesToString(rspData), "code").Int() != 200 {
 				ctx.SendChain(message.Text("出错了联系管理员看看吧"))
 				return
