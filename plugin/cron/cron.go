7826c6ec1f8375b4a2ad93c1f1687bf943fe7989
韩家傲（ChrisHan） <hanjiaao@qiyi.com>
2022-08-15 16:06:41 +0800

    地震查询使用中国地震台网数据

--- plugin/cron/cron.go
+++ plugin/cron/cron.go
@@ -1,32 +1,29 @@
 package cron
 
 import (
-	"bytes"
-	"encoding/json"
 	"fmt"
-	"runtime"
-
-	"github.com/FloatTech/zbputils/binary"
-	"github.com/FloatTech/zbputils/process"
-
+	"github.com/FloatTech/ZeroBot-Plugin/util"
 	ctrl "github.com/FloatTech/zbpctrl"
+	"github.com/FloatTech/zbputils/binary"
 	"github.com/FloatTech/zbputils/control"
+	"github.com/FloatTech/zbputils/process"
 	"github.com/FloatTech/zbputils/web"
 	"github.com/fumiama/cron"
+	"github.com/gogo/protobuf/sortkeys"
 	"github.com/golang-module/carbon/v2"
 	log "github.com/sirupsen/logrus"
 	"github.com/tidwall/gjson"
 	zero "github.com/wdvxdr1123/ZeroBot"
 	"github.com/wdvxdr1123/ZeroBot/message"
-
-	"github.com/FloatTech/ZeroBot-Plugin/util"
+	"strings"
 )
 
 const (
 	ServiceName = "cron"
 )
 
-var history = make(map[int64]struct{}, 128)
+//var history = make(map[int64]struct{}, 128)
+var history []int64
 
 var last = carbon.Now().Timestamp() - carbon.Now().Timestamp()%60
 
@@ -39,7 +36,7 @@ func init() { // 一些定时器
 		c.Start()
 	}
 	control.Register(ServiceName, &ctrl.Options[*zero.Ctx]{
-		DisableOnDefault: false,
+		DisableOnDefault: true,
 		Help:             "- 一些定时任务\n- 目前有地震播报功能",
 	})
 }
@@ -62,40 +59,85 @@ func sendMessage30s() {
 	})
 }
 
+//func sendEarthquake(ctx *zero.Ctx, grpIds []int64) {
+//	now := carbon.Now().Timestamp()
+//	data, _ := json.Marshal(map[string]string{
+//		"action":     "requestMonitorDataAction",
+//		"dataSource": "CEIC",
+//		"startTime":  util.Interface2String(last * 1000),
+//		"endTime":    util.Interface2String(now * 1000),
+//	})
+//	// "http://www.ceic.ac.cn/ajax/speedsearch?page=1&&num=6"
+//	// data, _ := web.GetData("http://www.ceic.ac.cn/ajax/speedsearch?page=1&&num=6")
+//	// gjson.Get(strings.Trim(binary.BytesToString(data), "()"), "shuju")
+//	rspData, err := web.PostData("http://api.dizhensubao.getui.com/api.htm", "application/json", bytes.NewReader(data))
+//	if err != nil {
+//		log.Errorln("cron error ", err)
+//		return
+//	}
+//	strData := binary.BytesToString(rspData)
+//	for _, d := range gjson.Get(strData, "values").Array() {
+//		last = now
+//		_, ok := provinces[d.Get("loc_province").String()]
+//		_, hisOk := history[d.Get("time").Int()]
+//		lv := d.Get("mag").Float()
+//		if ok && lv >= 3.5 && !hisOk {
+//			for _, grpId := range grpIds {
+//				ctx.SendGroupMessage(grpId, []message.MessageSegment{
+//					message.Text(fmt.Sprintf("检测到 %s 于 "+carbon.CreateFromTimestamp(d.Get("time").Int()/1000).ToDateTimeString()+" 发生 %.1f 级地震，请处于震中位置人员注意安全~", d.Get("loc_name").String(), lv)),
+//				})
+//				process.SleepAbout1sTo2s()
+//			}
+//			history[d.Get("time").Int()] = struct{}{}
+//			if len(history) > 128 {
+//				history = nil // 防止无限增长
+//				runtime.GC()
+//			}
+//		}
+//	}
+//} 883734530
+
 func sendEarthquake(ctx *zero.Ctx, grpIds []int64) {
-	now := carbon.Now().Timestamp()
-	data, _ := json.Marshal(map[string]string{
-		"action":     "requestMonitorDataAction",
-		"dataSource": "CEIC",
-		"startTime":  util.Interface2String(last * 1000),
-		"endTime":    util.Interface2String(now * 1000),
-	})
 	// "http://www.ceic.ac.cn/ajax/speedsearch?page=1&&num=6"
-	// data, _ := web.GetData("http://www.ceic.ac.cn/ajax/speedsearch?page=1&&num=6")
-	// gjson.Get(strings.Trim(binary.BytesToString(data), "()"), "shuju")
-	rspData, err := web.PostData("http://api.dizhensubao.getui.com/api.htm", "application/json", bytes.NewReader(data))
+	data, err := web.GetData("http://www.ceic.ac.cn/ajax/speedsearch?page=1&&num=1")
 	if err != nil {
-		log.Errorln("cron error ", err)
 		return
 	}
-	strData := binary.BytesToString(rspData)
-	for _, d := range gjson.Get(strData, "values").Array() {
-		last = now
-		_, ok := provinces[d.Get("loc_province").String()]
-		_, hisOk := history[d.Get("time").Int()]
-		lv := d.Get("mag").Float()
-		if ok && lv >= 3.5 && !hisOk {
+	earth := gjson.Get(strings.Trim(binary.BytesToString(data), "()"), "shuju").Array()
+	count := len(history)
+	for _, earthData := range earth {
+		id := earthData.Get("id").Int()
+		if count != 0 && !sliceFind(id) && regionFind(earthData.Get("LOCATION_C").String()) && earthData.Get("M").Float() > 3.5 {
 			for _, grpId := range grpIds {
 				ctx.SendGroupMessage(grpId, []message.MessageSegment{
-					message.Text(fmt.Sprintf("检测到 %s 于 "+carbon.CreateFromTimestamp(d.Get("time").Int()/1000).ToDateTimeString()+" 发生 %.1f 级地震，请处于震中位置人员注意安全~", d.Get("loc_name").String(), lv)),
+					message.Text(fmt.Sprintf("检测到 %s 于 %s 发生 %.1f 级地震，请处于震中位置人员注意安全~", earthData.Get("LOCATION_C").String(), earthData.Get("O_TIME").String(), earthData.Get("M").Float())),
 				})
 				process.SleepAbout1sTo2s()
 			}
-			history[d.Get("time").Int()] = struct{}{}
-			if len(history) > 128 {
-				history = nil // 防止无限增长
-				runtime.GC()
-			}
+		}
+		history = append(history, id)
+	}
+	util.SliceDeduplicate(&history)
+	if len(history) > 12 {
+		sortkeys.Int64s(history)
+		history = history[len(history)-10:]
+	}
+}
+
+func sliceFind(id int64) bool {
+	for _, h := range history {
+		if h == id {
+			return true
 		}
 	}
+	return false
+}
+
+func regionFind(str string) bool {
+	for key, _ := range provinces {
+		if strings.Contains(str, key) {
+			return true
+		}
+	}
+	return false
 }
